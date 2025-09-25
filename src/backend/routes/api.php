052c6a9764b2740280760455b0ad761dbc32e7fd
ATEF GHAYAD <abohusam.ja.555@gmail.com>
2025-09-25 13:33:43 +0300

    added API for login

---
+++ src/backend/routes/api.php
@@ -0,0 +1,7 @@
+// routes/api.php
+
+use App\Http\Controllers\Api\AuthController;
+
+Route::post('/login', [AuthController::class, 'login']);
+// You'll also need a route to register users:
+Route::post('/register', [AuthController::class, 'register']);
