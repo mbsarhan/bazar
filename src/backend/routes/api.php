0aae6b971c86789b3f6e6778c9ca071e2ce2f704
ATEF GHAYAD <abohusam.ja.555@gmail.com>
2025-10-02 13:27:36 +0300

    working Update Password Api

--- src/backend/routes/api.php
+++ src/backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController; // Add this line
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,4 +24,6 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('/user', [AuthController::class, 'user'])->name('user');
+        // --- 2. ADD THIS NEW ROUTE FOR UPDATING THE PASSWORD ---
+    Route::post('/user/password', [UserController::class, 'updatePassword']);
 });
