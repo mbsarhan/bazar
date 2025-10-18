41311834990c95fd43048fb2511b53b3b96a5ecd
ATEF GHAYAD <abohusam.ja.555@gmail.com>
2025-10-18 15:38:23 +0300

    made the price integer

--- src/backend/app/Http/Resources/AdvertisementResource.php
+++ src/backend/app/Http/Resources/AdvertisementResource.php
@@ -35,7 +35,7 @@ class AdvertisementResource extends JsonResource
         $baseData = [
             'id'                => $this->id,
             'title'             => $this->title,
-            'price'             => number_format($this->price),
+            'price'             => $this->price,
             'location'          => "{$this->governorate}, {$this->city}",
             'description'       => $this->description,
             'status'            => $this->ad_status,
